fd6b940001f8d8d66c14a63dbae4475bed9750a2
b_b <bruno@eliaz.fr>
2020-09-10 15:20:05 +0000

    gros paquet
    la base de la home + la nav en mode responsive tout le temps + les typos + etc.

--- pz_options.php
+++ pz_options.php
@@ -4,3 +4,13 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 define('_DELAI_RECUPERER_URL_CACHE', 3600 * 30);
 define('_PZ_SCOPUS_KEY', '63e059beee265eda0bdbafe5df5606ef');
+
+$GLOBALS['z_blocs'] = array(
+	'content',
+	'extra',
+	'head',
+	'head_js',
+	'header',
+	'footer',
+	'breadcrumb',
+);
