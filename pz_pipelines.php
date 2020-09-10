e3f9ef035ef81a48ea5af5122b9e5f55b6511870
b_b <bruno@eliaz.fr>
2020-09-10 15:18:53 +0000

    permettre de filtrer le json de la carte en fonction d'une collection zitems

--- pz_pipelines.php
+++ pz_pipelines.php
@@ -14,3 +14,16 @@ function pz_taches_generales_cron($taches_generales) {
 	$taches_generales['pz'] = 60*60;
 	return $taches_generales;
 }
+
+/**
+ * Insertion dans le pipeline gis_modele_parametres_autorises (GIS)
+ * 
+ * Permettre de passer le paramètre type_auteur au modèle GIS
+ * 
+ * @param array $flux Le contexte d'environnement du pipeline
+ * @return array $flux Le contexte d'environnement modifié
+ */
+function resa_gis_modele_parametres_autorises($flux){
+	$flux[] = 'id_zcollection';
+	return $flux;
+}
