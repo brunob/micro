3f5fd7cec316d62df1044799ca3fbba23ca0d207
b_b <bruno@eliaz.fr>
2020-09-23 07:48:38 +0000

    WIP : la page publications et sa carte filtrable en fonction des zcollections
    ça bougera peut-être sur une page map si on souhaite afficher la liste textuelle des publications
    
    TODO : 
    
    - afficher la publication dans un panel au clic sur un marker
    - peut être des filtres avec checkbox à la biolitmap
    - peut-être des filtres en fonction des principaux tags
    - etc

--- lang/pz_en.php
+++ lang/pz_en.php
@@ -4,6 +4,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	'label_collections_toutes' => 'All collections',
+
 	'titre_programme' => 'Programme',
 	'titre_publications' => 'Publications',
 
