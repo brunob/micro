91d30fe7ee29e86d66a398d59c53e6295f4f188a
b_b <bruno@eliaz.fr>
2020-09-23 14:55:43 +0000

    item de langue

--- lang/pz_en.php
+++ lang/pz_en.php
@@ -4,6 +4,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	'lien_map' => 'View Map',
+
 	'label_collections_toutes' => 'All collections',
 
 	'titre_programme' => 'Programme',
