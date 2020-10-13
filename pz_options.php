3795336e8bb36f271b82f620f21c92c5524626f7
b_b <bruno@eliaz.fr>
2020-10-13 18:30:55 +0000

    move prefs top options

--- pz_options.php
+++ pz_options.php
@@ -2,6 +2,11 @@
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
+## valeurs modifiables dans mes_options
+if (!defined('_PZ_GENIE_NB_ITEMS')) {
+	define('_PZ_GENIE_NB_ITEMS', 50);
+}
+
 define('_DELAI_RECUPERER_URL_CACHE', 3600 * 30);
 define('_PZ_SCOPUS_KEY', '63e059beee265eda0bdbafe5df5606ef');
 
