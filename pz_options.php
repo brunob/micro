42683c4a18a21d3665a1745ac90321acab8d133b
brunob <bruno@eliaz.fr>
2021-01-13 13:41:53 +0100

    cleanup

--- pz_options.php
+++ pz_options.php
@@ -8,7 +8,6 @@ if (!defined('_PZ_GENIE_NB_ITEMS')) {
 }
 
 define('_DELAI_RECUPERER_URL_CACHE', 3600 * 30);
-define('_PZ_SCOPUS_KEY', '63e059beee265eda0bdbafe5df5606ef');
 
 $GLOBALS['z_blocs'] = array(
 	'content',
